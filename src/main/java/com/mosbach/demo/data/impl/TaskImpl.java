bf6d89408c8a8d2b5043e9efb251047fd748b89b
Michael Hartwig <michael.hartwig@commercetools.com>
2024-09-03 07:18:14 +0200

    Init on maven for new year

--- src/main/java/com/mosbach/demo/data/impl/TaskImpl.java
+++
@@ -1,7 +0,0 @@
-package com.mosbach.demo.data.impl;
-
-
-import com.mosbach.demo.data.api.Task;
-
-public class TaskImpl {
-}
--- src/main/java/com/mosbach/demo/data/impl/UserImpl.java
+++
@@ -1,6 +0,0 @@
-package com.mosbach.demo.data.impl;
-
-import com.mosbach.demo.data.api.User;
-
-public class UserImpl {
-}
